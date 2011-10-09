c4e706951c8a52a9d5023549743d5da813d02234
Forrest Shi <Forrest@fshi.local>
2011-10-09 16:05:50 +0800

    first ci

--- TestRunLoop/ViewController.m
+++ TestRunLoop/ViewController.m
@@ -22,8 +22,62 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // This can not work ( show the activity view ) 
+    
+     activityIndicator = [[UIActivityIndicatorView alloc]
+                          initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    
+    [self.view addSubview:activityIndicator];
+    
+    [activityIndicator startAnimating];
+    NSLog(@"started animation..");
+
+    
+    [[NSRunLoop currentRunLoop] runUntilDate:[NSDate date]];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.twitter.com"]];
+    NSURLResponse *response;
+    NSError *error;
+    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if (error) {
+        NSLog(@"error is %@",error);
+    }else{
+        NSLog(@"No error with response %@", response);
+    }
+    NSLog(@"stop animation..");
+    [activityIndicator stopAnimating];
+    
+    //HOW TO FIX ?
+    
+    //SOLUTION 1
+    
+//    activityIndicator = [[UIActivityIndicatorView alloc]
+//                                                  initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+//    
+//    [self.view addSubview:activityIndicator];
+//    [activityIndicator startAnimating];
+//    
+//    //[self performSelector:@selector(sendRequest:) withObject:nil];
+//    [self performSelector:@selector(sendRequest:) withObject:nil afterDelay:5];
+
 }
 
+- (void) sendRequest:(id)sender{
+    NSLog(@"%s",__PRETTY_FUNCTION__);
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.google.com"]];
+    NSURLResponse *response;
+    NSError *error;
+    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if (error) {
+        NSLog(@"error is %@",error);
+    }else{
+        NSLog(@"No error with response %@", response);
+    }
+    NSLog(@"stop animation..");
+    [activityIndicator stopAnimating];
+}
 - (void)viewDidUnload
 {
     [super viewDidUnload];
