e8b3df1b089cb6b8237e278544722da0508990fa
Forrest Shi <Forrest@fshi.local>
2011-10-09 15:14:31 +0800

    Initial Commit

---
+++ TestRunLoop/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestRunLoop
+//
+//  Created by Forrest Shi on 09/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestRunLoop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestRunLoop
+//
+//  Created by Forrest Shi on 09/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
